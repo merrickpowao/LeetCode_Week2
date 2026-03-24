6f7cd6c2006870511d9fe4ec86316f04ce45ad80
Karl Merrick Powao <merrickhaspower@gmail.com>
2026-03-24 17:33:24 +0000

    Solution

---
+++ Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args){
+
+    }
+
+    public static boolean hasCycle(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+
+            if (fast == slow) {
+                return true;
+            }
+        }
+
+        return false;        
+    }
+}
